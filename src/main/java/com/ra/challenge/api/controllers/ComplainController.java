593f87d068713563e49a670520804b58626bd7f3
Karinna Dias da Silva <kads@br.ibm.com>
2018-08-03 13:57:59 -0300

    RA Challenge Karinna Dias

--- src/main/java/com/ra/challenge/api/controllers/ComplainController.java
+++ src/main/java/com/ra/challenge/api/controllers/ComplainController.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.mockito.internal.matchers.Find;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mongodb.BasicDBObject;
